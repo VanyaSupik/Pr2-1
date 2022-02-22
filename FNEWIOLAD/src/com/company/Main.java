e51f757c5a6f4f01c5e7da34b24f3d0848fa4042
VanyaSupik <99318679+VanyaSupik@users.noreply.github.com>
2022-02-22 13:41:45 +0200

    Add files via upload

---
+++ FNEWIOLAD/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b= 5;
+        int c=a/b;
+        int f=a%b;
+        System.out.println(" "+c+" "+f);
+
+     int d = 12;
+     int g = d/10;
+     int h = d%10;
+     System.out.println("sum :"+(g+h));
+
+     double hh = 2.64567;
+     System.out.println((int)Math.round(hh));
+    }
+}
